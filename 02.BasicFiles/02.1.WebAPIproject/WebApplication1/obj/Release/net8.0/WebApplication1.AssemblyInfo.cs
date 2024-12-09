2bc76d599a404e9483ff5917322a2f9b66308e3b
ManthanThakor <thakormanthan849@gmail.com>
2024-12-09 16:46:24 +0530

    change

--- 02.BasicFiles/02.1.WebAPIproject/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ 02.BasicFiles/02.1.WebAPIproject/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc9f4279a22ff386fb40da46f9e791f22ac9302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e33c367509bcadaa72078e80bbba3061284432f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 02.BasicFiles/02.2.WebApplicationwithMVC/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ 02.BasicFiles/02.2.WebApplicationwithMVC/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d2fb6d9010dd3096732c11450d6b110906a68b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e33c367509bcadaa72078e80bbba3061284432f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
