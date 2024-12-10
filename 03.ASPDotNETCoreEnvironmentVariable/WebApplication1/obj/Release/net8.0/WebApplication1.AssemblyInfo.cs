3f47ca365b20fa7253595b6efa9f10cfc0b5216f
ManthanThakor <thakormanthan849@gmail.com>
2024-12-10 17:58:43 +0530

    change

--- 03.ASPDotNETCoreEnvironmentVariable/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ 03.ASPDotNETCoreEnvironmentVariable/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08a9865945e511e1a51115c7b152051ef1e8198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a80679241eb0a08d2603f89d40e2eaeef71fef2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
